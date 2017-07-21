8935455f7419d768632a2eaa8dd108928f21abc0
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2017-07-21 11:57:55 -0400

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        DM Toolkit
+      </a>
+      <!-- Top Menu Items for mobile
+
+      <a class="navbar-item is-hidden-desktop" href="https://github.com/jgthms/bulma" target="_blank">
+        <span class="icon" style="color: #333;">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+
+      -->
+    </div>
+    <div class="navbar-burger burger" data-target="navMenuExample">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="navbar-menu">
+
+    </div>
+
+  </nav>
+
+</template>
+
+<script>
+
+</script>
