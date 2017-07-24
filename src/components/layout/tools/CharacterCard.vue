40e94e65f6f1582d81f947b0e92816de68c253a8
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2017-07-24 14:06:38 -0400

    Beginning work on initiative tracker.

---
+++ src/components/layout/tools/CharacterCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="character-card">
+    <h3 class="character-name">Player Name</h3>
+    <h5 class="initiative">Initiative</h5>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'charactercard'
+  }
+</script>
+
+<style>
+</style>
--- src/components/layout/tools/Tracker.vue
+++ src/components/layout/tools/Tracker.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="tracker-tray">
     Tracker
+    <charactercard />
   </div>
 </template>
+
 <script>
+  import CharacterCard from './CharacterCard'
   export default {
-    name: 'tracker'
+    name: 'tracker',
+    components: {
+      'charactercard': CharacterCard
+    }
   }
 </script>
+
 <style>
 </style>
