b5a03150a16f62c5739877154bb5804836fdb5e8
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2017-08-21 18:33:26 -0400

    Adding notes/note component.

---
+++ src/components/layout/tools/Note.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note">
+    Noteeeeee
+  </div>
+</template>
+<script>
+  export default {
+    name: 'note'
+  }
+</script>
+
+<style>
+  .note {
+    background-color: rgba(255,255,255,0.5);
+    border-radius: 1em;
+    padding: .5em;
+  }
+</style>
--- src/components/layout/tools/Notes.vue
+++ src/components/layout/tools/Notes.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="notes-tray">
-    Notes
+    <div class="header">Notes</div>
+    <div class="tab-control">
+      Pseudo-tab
+    </div>
+    <div class="note-container">
+      <note />
+    </div>
   </div>
 </template>
 <script>
+  import Note from './Note'
   export default {
-    name: 'notes'
+    name: 'notes',
+    components: {
+      'note': Note
+    }
   }
 </script>
 <style>
+  .notes-tray {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    /*justify-content: center;*/
+  }
+  .header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    border-bottom: 1px solid magenta;
+    height: 2em;
+    align-content: center;
+  }
+  .tab-control {
+    display: flex;
+    width: 100%;
+    height: 2em;
+  }
+  .note-container {
+    /*height: 100%;*/
+    width: 100%;
+  }
 </style>
